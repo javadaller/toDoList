32c9558686d670e0624bb359e84ee776eb6324bd
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-04-30 12:01:44 +0200

    local storage

--- assets/js/fnc.js
+++ assets/js/fnc.js
@@ -1,66 +1,65 @@
 //CREATE LIST
-export async function createList(list) {
-  const name=escapeHTML(list.name);
+export async function createList(inputName,checked,newTask) {
+  const listContainer = document.querySelector('#listContainer');
+  const error = document.querySelector('#inputError');
 
-  if (name=='') {
+  const name = escapeHTML(inputName);
+
+  const newIndex = listContainer.children.length;
+  const taskID = generateID(name, newIndex);
+
+  if (name == '') {
 
       taskName.classList.add('error');
-      error.innerText='the name is empty';
+      error.innerText = 'the name is empty';
 
   } else if(name.length<20) {
 
-      addTaskOpen=false;
-      addTask.style.display='none';
+      addTask.style.display = 'none';
 
       //container
-      const taskContainer=createDiv('li',listContainer,null,'task');
+      const taskContainer = createDiv('li',listContainer,null,'task');
+      taskContainer.id = taskID;
 
       //drag svg
-      const taskDrag=createDiv('img',taskContainer,null,'svgDrag');
-      taskDrag.src="assets/images/icons/draggable.svg";
+      const taskDrag = createDiv('img',taskContainer,null,'svgDrag');
+      taskDrag.src = "assets/images/icons/draggable.svg";
 
       let draggingElement = null;
 
       taskDrag.addEventListener('mousedown', (event) => {
-          event.preventDefault(); // Empêcher la sélection de texte
+          event.preventDefault();
 
           draggingElement = taskContainer;
 
-          // Sauvegarder la position initiale de l'élément
+          //initial position
           const initialOffset = event.clientY - draggingElement.getBoundingClientRect().top;
-
-          // Événement mousemove : déplacer l'élément
           document.addEventListener('mousemove', onMouseMove);
 
           function onMouseMove(event) {
               if (draggingElement) {
-                  // Calculer la nouvelle position de l'élément
                   const newY = event.clientY - initialOffset*10;
-
-                  // Appliquer la translation à l'élément
                   draggingElement.style.transform = `translateY(${newY}px)`;
               }
           }
 
-          // Événement mouseup : arrêter le déplacement
+          //stop drag
           document.addEventListener('mouseup', onMouseUp);
 
           function onMouseUp(event) {
               if (draggingElement) {
-                  // Réinitialiser l'élément en cours de déplacement
                   draggingElement.style.transform = 'none';
                   draggingElement = null;
 
-                  // Trouver la nouvelle position de l'élément dans la liste
                   const newIndex = calculateNewIndex(event.clientY);
 
-                  // Réinsérer l'élément à sa nouvelle position
+                  //insert into new position
                   if (newIndex !== -1) {
                       const referenceNode = listContainer.children[newIndex];
                       listContainer.insertBefore(taskContainer, referenceNode);
                   }
 
-                  // Supprimer les gestionnaires d'événements mousemove et mouseup
+                  //remove up and move
                   document.removeEventListener('mousemove', onMouseMove);
                   document.removeEventListener('mouseup', onMouseUp);
               }
@@ -76,50 +75,63 @@ export async function createList(list) {
                   return i;
               }
           }
-
           return listItems.length;
       }
 
       //task name
-      const taskContainerName=createDiv('p',taskContainer,name,'taskName');
+      const taskContainerName = createDiv('p',taskContainer,name,'taskName');
 
       //task check
-      const taskCheck=createDiv('input',taskContainer,'taskCheck');
-      taskCheck.type='checkbox';
-      checkTask(taskCheck,taskContainerName);
+      const taskCheck = createDiv('input',taskContainer,'taskCheck');
+      taskCheck.type = 'checkbox';
+      if(checked != null) {
+        taskCheck.checked = checked;
+        if(checked) {
+          taskContainerName.style.textDecoration = 'line-through';
+          taskContainerName.style.color = 'grey';
+        } else {
+          taskContainerName.style.textDecoration = 'none';
+          taskContainerName.style.color = 'inherit';
+        }
+      }
+      checkTask(taskCheck,taskContainerName,taskID);
 
       //task delete
-      const taskDelete=createDiv('img',taskContainer,null,'svgDelete');
-      taskDelete.src="assets/images/icons/remove.svg";
-      deleteTask(taskContainer,taskDelete);
+      const taskDelete = createDiv('img',taskContainer,null,'svgDelete');
+      taskDelete.src = "assets/images/icons/remove.svg";
+      deleteTask(taskID,taskDelete);
 
       //update storage
-      const newTask = { name: name, checked: false };
-      let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-      tasks.push(newTask);
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-
+      if(newTask) {
+        const newTask = {id:taskID, name: name, checked: taskCheck.checked };
+        let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+        tasks.push(newTask);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
+      
       //reset
-      taskName.value='';
+      taskName.value = '';
       taskName.classList.remove('error');
       taskName.classList.add('noError');
-      error.innerText='';
+      error.innerText = '';
       await sleep(300);
-      list.style.display='block';
+      list.style.display = 'block';
 
   } else {
       taskName.classList.add('error');
-      error.innerText='20 characters maximum';
+      error.innerText = '20 characters maximum';
   }
-}
+} //end createList
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 // CREATE DIV
-export function createDiv(type,parent,content,className) {
-  const newDiv=document.createElement(type);
-  if (type!='') {
-    newDiv.innerHTML=content;
+function createDiv(type,parent,content,className) {
+  const newDiv = document.createElement(type);
+  if (type != '') {
+    newDiv.innerHTML = content;
   }
-  if (className!=null) {
+  if (className != null) {
     newDiv.classList.add(className);
   }
   parent.appendChild(newDiv);
@@ -135,7 +147,7 @@ export function sleep(ms) {
 //------------------------------------------------------------------------------------------
 
 // ESCAPE HTML
-export function escapeHTML(text) {
+function escapeHTML(text) {
   var map = {
     '&': '&amp;',
     '<': '&lt;',
@@ -149,15 +161,40 @@ export function escapeHTML(text) {
 
 //------------------------------------------------------------------------------------------
 
-// IS MOBILE
-function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+//DELETE TASK
+function deleteTask(ID,button) {
+  button.addEventListener('click', () => {
+    if (confirm("Are you sure to remove this task?")) {
+      document.querySelector('#'+ID).remove();
+    }
+  })
 }
 
 //------------------------------------------------------------------------------------------
 
-// RANDOM COLOR
-function randomColor() {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return '#'+randomColor;
+//CHECK TASK
+function checkTask(checkbox,task,ID) {
+  checkbox.addEventListener('change', () => {
+    const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    const taskToUpdate = tasks.find(task => task.id === ID);
+    taskToUpdate.checked = checkbox.checked;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    if(checkbox.checked) {
+        task.style.textDecoration = 'line-through';
+        task.style.color = 'grey';
+    } else {
+        task.style.textDecoration = 'none';
+        task.style.color = 'inherit';
+    }
+  })
 }
+
+//------------------------------------------------------------------------------------------
+
+//GENERATE ID
+function generateID(name, index) {
+  return name.replace(/\s/g, '')+'_'+index;
+}
+
+//------------------------------------------------------------------------------------------
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,198 +1,48 @@
-import {sleep,createDiv,escapeHTML,createList} from './fnc.js';
+import {sleep,createList} from './fnc.js';
 
 //init
-let addTaskOpen=false;
 const list=document.querySelector('#list');
 const addTask=document.querySelector('#addTask');
-const listContainer=document.querySelector('#listContainer');
 const taskName=document.querySelector('#taskName');
 const error=document.querySelector('#inputError');
 
 //local storage
-// let storage=localStorage.getItem('tasks');
-// if(storage!=null) {
-//     console.log('chargement taches');
-//     storage=localStorage.getItem('tasks');
-//     storage.forEach(element => {
-//         createList(element);
-//     });
-    
-// } else {
-//     storage=[];
-//     localStorage.setItem('tasks', storage);
-// }
+const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+if(tasks!=null) {
+    tasks.forEach(element => {
+        createList(element.name,element.checked,false);
+    });
+}
 
 //Add button
 const addButton=document.querySelector('#listAddButton');
 
 addButton.addEventListener('click', () => {
-    if (!addTaskOpen) {
-        addNewTask();
-    }
+    addNewTask();
 })
 
 //close button
 const closeButton=document.querySelector('#addTaskClose');
 
 closeButton.addEventListener('click', () => {
-    if(addTaskOpen) {
-        addTaskOpen=false;
-        addTask.style.display='none';
-        list.style.display='block';
-        taskName.classList.remove('error');
-        taskName.classList.add('noError');
-        error.innerText='';
-        taskName.value='';
-    }
+    addTask.style.display='none';
+    list.style.display='block';
+    taskName.classList.remove('error');
+    taskName.classList.add('noError');
+    error.innerText='';
+    taskName.value='';
 })
 
 //confirm button
 const confirmButton=document.querySelector('#confirmButton');
 
 confirmButton.addEventListener('click', () => {
-    confirmTask();
+    createList(taskName.value,null,true);
 })
 
+//add new task
 async function addNewTask() {
-    addTaskOpen=true;
     list.style.display='none';
     await sleep(300);
     addTask.style.display='block';
 }
-
-async function confirmTask() {
-    
-    const name=escapeHTML(taskName.value);
-
-    if (name=='') {
-        taskName.classList.remove('noError');
-        taskName.classList.add('error');
-        error.innerText='the name is empty';
-
-    } else if(name.length<20) {
-
-        addTaskOpen=false;
-        addTask.style.display='none';
-
-        //container
-        const taskContainer=createDiv('li',listContainer,null,'task');
-
-        //drag svg
-        const taskDrag=createDiv('img',taskContainer,null,'svgDrag');
-        taskDrag.src="assets/images/icons/draggable.svg";
-
-        let draggingElement = null;
-
-        taskDrag.addEventListener('mousedown', (event) => {
-            event.preventDefault(); // Empêcher la sélection de texte
-
-            draggingElement = taskContainer;
-
-            // Sauvegarder la position initiale de l'élément
-            const initialOffset = event.clientY - draggingElement.getBoundingClientRect().top;
-
-            // Événement mousemove : déplacer l'élément
-            document.addEventListener('mousemove', onMouseMove);
-
-            function onMouseMove(event) {
-                if (draggingElement) {
-                    // Calculer la nouvelle position de l'élément
-                    const newY = event.clientY - initialOffset*10;
-
-                    // Appliquer la translation à l'élément
-                    draggingElement.style.transform = `translateY(${newY}px)`;
-                }
-            }
-
-            // Événement mouseup : arrêter le déplacement
-            document.addEventListener('mouseup', onMouseUp);
-
-            function onMouseUp(event) {
-                if (draggingElement) {
-                    // Réinitialiser l'élément en cours de déplacement
-                    draggingElement.style.transform = 'none';
-                    draggingElement = null;
-
-                    // Trouver la nouvelle position de l'élément dans la liste
-                    const newIndex = calculateNewIndex(event.clientY);
-
-                    // Réinsérer l'élément à sa nouvelle position
-                    if (newIndex !== -1) {
-                        const referenceNode = listContainer.children[newIndex];
-                        listContainer.insertBefore(taskContainer, referenceNode);
-                    }
-
-                    // Supprimer les gestionnaires d'événements mousemove et mouseup
-                    document.removeEventListener('mousemove', onMouseMove);
-                    document.removeEventListener('mouseup', onMouseUp);
-                }
-            }
-        });
-
-        function calculateNewIndex(clientY) {
-            const listItems = [...listContainer.children];
-
-            for (let i = 0; i < listItems.length; i++) {
-                const rect = listItems[i].getBoundingClientRect();
-                if (clientY < rect.top + rect.height / 2) {
-                    return i;
-                }
-            }
-
-            return listItems.length;
-        }
-
-        //task name
-        const taskContainerName=createDiv('p',taskContainer,name,'taskName');
-
-        //task check
-        const taskCheck=createDiv('input',taskContainer,'taskCheck');
-        taskCheck.type='checkbox';
-        checkTask(taskCheck,taskContainerName);
-
-        //task delete
-        const taskDelete=createDiv('img',taskContainer,null,'svgDelete');
-        taskDelete.src="assets/images/icons/remove.svg";
-        deleteTask(taskContainer,taskDelete);
-
-        //update storage
-        const newTask = { name: name, checked: false };
-        let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-        tasks.push(newTask);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-
-        //reset
-        taskName.value='';
-        taskName.classList.remove('error');
-        taskName.classList.add('noError');
-        error.innerText='';
-        await sleep(300);
-        list.style.display='block';
-
-    } else {
-        taskName.classList.remove('noError');
-        taskName.classList.add('error');
-        error.innerText='20 characters maximum';
-    }
-}
-
-function deleteTask(parent,button) {
-    button.addEventListener('click', () => {
-        if (confirm("Are you sure to remove this task?")) {
-            parent.remove();
-        }
-    })
-}
-
-function checkTask(checkbox,task) {
-    checkbox.addEventListener('change', () => {
-        if(checkbox.checked) {
-            task.style.textDecoration='line-through';
-            task.style.color='grey';
-        } else {
-            task.style.textDecoration='none';
-            task.style.color='inherit';
-        }
-    })
-}
-
